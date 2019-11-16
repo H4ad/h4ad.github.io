25539a6bc7b56d109e7cfdea105c7d8ca69bfa51
Vinícius Lourenço <vinyygamerlol@gmail.com>
2019-11-16 14:30:51 -0300

    add(frontend): angular project for portfolio website

---
+++ frontend/src/app/app.component.ts
@@ -0,0 +1,15 @@
+//#region Imports
+
+import { Component } from '@angular/core';
+
+//#endregion
+
+/**
+ * A classe que representa a página principal da aplicação
+ */
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {}
