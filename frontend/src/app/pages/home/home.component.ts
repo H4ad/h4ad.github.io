f31a3115bac1f86e902d2b55e4d2f1d61c5c88f3
Vinícius Lourenço <vinyygamerlol@gmail.com>
2019-11-16 18:36:42 -0300

    update(pages): added home page with some content and style

---
+++ frontend/src/app/pages/home/home.component.ts
@@ -0,0 +1,15 @@
+//#region Imports
+
+import { Component } from '@angular/core';
+
+//#endregion
+
+/**
+ * A classe que representa a página que é exibida ao entrar no site
+ */
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
+})
+export class HomeComponent { }
