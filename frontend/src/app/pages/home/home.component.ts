c32773196f778e5453261c7b2abb71b5416438f1
Vinícius Lourenço <vinyygamerlol@gmail.com>
2019-11-16 22:14:34 -0300

    update(home): added content about me

--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -12,4 +12,15 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent { }
+export class HomeComponent {
+
+  //#region Constructor
+
+  /**
+   * Construtor padrão
+   */
+  constructor() { }
+
+  //#endregion
+
+}
