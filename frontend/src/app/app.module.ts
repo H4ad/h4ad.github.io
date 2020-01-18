876ea9a849baa08d9d5fc2b1aa67f70e55829f83
Vinícius Lourenço <vinyygamerlol@gmail.com>
2020-01-18 16:04:05 -0300

    added(http-async): wrapper service for http client with more functionalities

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,20 +1,26 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { httpAsyncFactory } from './factories/http-async/http-async.factory';
 import { HomeComponent } from './pages/home/home.component';
+import { HttpAsyncService } from './services/http-async/http-async.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [
+    { provide: HttpAsyncService, useFactory: httpAsyncFactory, deps: [HttpClient] },
+  ],
   bootstrap: [
     AppComponent,
   ],
