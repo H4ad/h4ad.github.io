7a884278d3e8fc6cecce5d196a075225017aecaa
Vinícius Lourenço <vinyygamerlol@gmail.com>
2020-01-20 21:34:53 -0300

    added(badge & project): entities

---
+++ backend/src/typeorm/entities/badge.entity.ts
@@ -0,0 +1,47 @@
+//#region Imports
+
+import { Column, Entity, JoinTable, ManyToMany } from 'typeorm';
+
+import { BaseEntity } from '../../models/base/base-entity';
+import { ProjectEntity } from './project.entity';
+
+//#endregion
+
+/**
+ * A classe que representa as informações de uma Badge
+ */
+@Entity()
+export class BadgeEntity extends BaseEntity {
+
+  //#region Constructor
+
+  /**
+   * Construtor padrão
+   */
+  constructor(partial: Partial<BadgeEntity>) {
+    super();
+
+    Object.assign(this, partial);
+  }
+
+  //#endregion
+
+  /**
+   * A imagem do projeto
+   */
+  @Column({ nullable: false })
+  imageUrl: string;
+
+  /**
+   * O texto alternativo para a imagem do projeto
+   */
+  @Column({ nullable: false })
+  imageAlt: string;
+
+  /**
+   * A lista de projetos que essa insigna possui
+   */
+  @ManyToMany(() => ProjectEntity, project => project.badges)
+  projects: ProjectEntity[];
+
+}
