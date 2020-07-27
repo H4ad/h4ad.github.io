07e77593a32112ae594604a09b9a94dac0884a58
Vinícius Lourenço <vinyygamerlol@gmail.com>
2020-07-26 22:35:20 -0300

    feat(github): added integration to publish on github pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,7 +21,7 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-google-analytics',
       options: {
-        trackingId: 'UA-76055934-8',
+        trackingId: 'G-MMKNPDE0CK',
       },
     },
   ],
